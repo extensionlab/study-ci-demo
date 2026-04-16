d08dbbd0100c816f79e3d48454a3b34987e2a38e
hdeg <76611318+exit1991@users.noreply.github.com>
2026-04-16 21:32:18 +0900

    失敗パターンを作成

--- src/sum.ts
+++ src/sum.ts
@@ -3,9 +3,9 @@ export function sum(a: number, b: number): number {
     
     // 検証用: バグを意図的に挿入
     // 第2引数が77の場合、強制的に777を返却
-    // if (b === 77) {
-    //     return 777;
-    // }
+    if (b === 77) {
+        return 777;
+    }
   
     return a + b;
 }
